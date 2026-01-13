442d01f02bd1ea6e4b04259a4beb8b1129db3e57
Camilo Vega <camilo_25_00@hotmail.com>
2026-01-12 23:51:19 -0500

    subida

--- src/android/CaptureFingerprintActivity.java
+++ src/android/CaptureFingerprintActivity.java
@@ -35,7 +35,6 @@ import com.digitalpersona.uareu.UareUGlobal;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import com.outsystemsenterprise.entel.PEMayorista.R;
