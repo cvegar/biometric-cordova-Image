fe9678cb003b284b7d9fe57d8362a640400c428c
Camilo Vega <camilo_25_00@hotmail.com>
2026-01-14 02:30:53 -0500

    subida

--- src/android/CaptureFingerprintActivity.java
+++ src/android/CaptureFingerprintActivity.java
@@ -585,7 +585,7 @@ public class CaptureFingerprintActivity extends Activity implements OnItemSelect
 
             }
             m_reader.Close();
-
+                
             Intent i = new Intent();
             i.putExtra("device_name", m_deviceName);
             i.putExtra("finger", finalwsq);
@@ -595,13 +595,16 @@ public class CaptureFingerprintActivity extends Activity implements OnItemSelect
             i.putExtra("extra",scorePAD);
             i.putExtra("product",productName);
             i.putExtra("vendor",vendorName);
+            Log.i(LOG_TAG, "onBackPressed - finalwsq:" + (finalwsq != null ? "OK (len=" + finalwsq.length() + ")" : "NULL"));
+            Log.i(LOG_TAG, "onBackPressed - minutia:" + (minutia != null ? "OK (len=" + minutia.length() + ")" : "NULL"));
+            Log.i(LOG_TAG, "onBackPressed - fingerPngB64:" + (fingerPngB64 != null ? "OK (len=" + fingerPngB64.length() + ")" : "NULL"));
             Log.i(LOG_TAG, "Imagen Base64=" + fingerPngB64);
-            //i.putExtra("finger_png_b64", fingerPngB64);
+            i.putExtra("finger_png_b64", fingerPngB64);
             setResult(Activity.RESULT_OK, i);
             finish();
 
         } catch (Exception e) {
-            Log.w("UareUSampleJava", "error during reader shutdown");
+            Log.i("UareUSampleJava", "error during reader shutdown");
             //Utils.saveErrorInStorage("Error during reader shutdown");
 
             Intent i = new Intent();
@@ -614,7 +617,7 @@ public class CaptureFingerprintActivity extends Activity implements OnItemSelect
             i.putExtra("product",productName);
             i.putExtra("vendor",vendorName);
             Log.i(LOG_TAG, "Imagen Base64=" + fingerPngB64);
-            //i.putExtra("finger_png_b64", fingerPngB64);
+            i.putExtra("finger_png_b64", fingerPngB64);
             setResult(Activity.RESULT_OK, i);
             finish();
         }
