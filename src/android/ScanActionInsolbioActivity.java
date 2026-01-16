2a050dc5dac2ba430a6b231e8b93e83f4e4b4592
Vega Rioja Camilo Rodolfo <camilo_25_00@hotmail.com>
2026-01-16 17:08:19 -0500

    subida lap

--- src/android/ScanActionInsolbioActivity.java
+++ src/android/ScanActionInsolbioActivity.java
@@ -200,7 +200,7 @@ public class ScanActionInsolbioActivity extends Activity {
                     //imgBase64String = CryptoUtil.encrypt_(Base64.encodeToString(pngBytes, Base64.NO_WRAP));
                     Log.i(TAG, "base64IMG: " + imgBase64String);
                     Log.i(TAG, "raw len=" + (raw == null ? 0 : raw.length));    
-                    intent.putExtra("pngbBytes", pngBytes);
+                    //intent.putExtra("pngbBytes", pngBytes);
                     intent.putExtra("raw_imag", raw);
                     encriptedBase64 = CryptoUtil.encrypt_(data.getStringExtra("finger"));
                     encriptedMinutia = CryptoUtil.encrypt_(data.getStringExtra("minutia"));
